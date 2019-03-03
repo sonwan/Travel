8dbee39917e0d6a0387befa063eb4ba5e10e4855
sonwan <15022693687@163.com>
2019-03-03 15:18:06 +0800

    添加jar包

---
+++ app/src/main/java/com/tj/graduation/travel/MyApplication.java
@@ -0,0 +1,15 @@
+package com.tj.graduation.travel;
+
+import android.app.Application;
+
+/**
+ * Created by wangsong on 2019/3/3.
+ */
+
+public class MyApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
