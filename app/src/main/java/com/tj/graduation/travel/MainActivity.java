9504c438af6f2d720734641c417c4858035bf883
sonwan <15022693687@163.com>
2019-03-03 11:19:58 +0800

    Revert "添加网络请求"
    This reverts commit b4f1a4c841543ff70bd107dff266a2494461de00.

--- app/src/main/java/com/tj/graduation/travel/MainActivity.java
+++ app/src/main/java/com/tj/graduation/travel/MainActivity.java
@@ -1,59 +1,14 @@
 package com.tj.graduation.travel;
 
+import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.TextView;
-import android.widget.Toast;
 
-import com.tj.graduation.travel.base.BaseActivity;
-import com.tj.graduation.travel.model.SpotListModel;
-import com.tj.graduation.travel.util.http.RequestUtil;
-import com.tj.graduation.travel.util.http.listener.DisposeDataListener;
-import com.tj.graduation.travel.util.http.request.RequestParams;
-
-public class MainActivity extends BaseActivity {
-
-    private TextView tv;
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
-        Button btn = findViewById(R.id.btn);
-        tv = findViewById(R.id.tv);
-        btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                doRequest(new DisposeDataListener() {
-                    @Override
-                    public void onSuccess(Object responseObj) {
-
-                        SpotListModel model = (SpotListModel) responseObj;
-                        Toast.makeText(MainActivity.this, model.getMsg(), Toast.LENGTH_LONG).show();
-
-                    }
-
-                    @Override
-                    public void onFailure(Object responseObj) {
-
-                    }
-                });
-            }
-        });
-
     }
-
-    private void doRequest(DisposeDataListener listener) {
-        RequestParams params = new RequestParams();
-
-        params.put("curpagenum", "1");
-        params.put("pagecount", "10");
-
-        RequestUtil.getRequest(Constant.URL + "querySpotList.api", params, listener, SpotListModel.class);
-    }
-
-
 }
