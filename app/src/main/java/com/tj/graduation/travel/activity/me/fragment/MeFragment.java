81e64486885fb11a6539ad59b510565883a63e1d
w820538874 <48150480+w820538874@users.noreply.github.com>
2019-03-06 00:45:15 +0800

    我的页面
    我的

--- app/src/main/java/com/tj/graduation/travel/activity/me/fragment/MeFragment.java
+++ app/src/main/java/com/tj/graduation/travel/activity/me/fragment/MeFragment.java
@@ -1,10 +1,17 @@
 package com.tj.graduation.travel.activity.me.fragment;
 
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.PorterDuff;
+import android.graphics.PorterDuffXfermode;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.tj.graduation.travel.R;
 import com.tj.graduation.travel.base.BaseFragment;
@@ -16,6 +23,9 @@ import com.tj.graduation.travel.base.BaseFragment;
 
 public class MeFragment extends BaseFragment {
 
+    private ImageView img_tx;
+    private TextView user_name;
+
     public static MeFragment newInstance(){
         MeFragment meFragment = new MeFragment();
         return meFragment;
@@ -24,8 +34,10 @@ public class MeFragment extends BaseFragment {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.me_fragment,container,false);
+        View view = inflater.inflate(R.layout.me_fragment,container,false);
+        img_tx = view.findViewById(R.id.h_head);
+        user_name = view.findViewById(R.id.user_name);
+        return view;
     }
-
 
 }
